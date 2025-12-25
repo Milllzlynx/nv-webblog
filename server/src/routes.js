06c59eb50ce5d771e3ce0b87210a3d989a534ca5
- <->
2025-12-25 16:55:09 +0700

    updates

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,21 +1,21 @@
 const UserController = require('./controllers/UserController')
+const AuthenticationController = require('./controllers/AuthenticationController')
 
-module.exports = (app) => {
-    /* RESFUL Api for users management */
-    
+module.exports = (app) => { 
     // create user
     app.post('/user', UserController.create)
-
     // edit user, suspend, active
     app.put('/user/:userId', UserController.put)
-
     // delete user
     app.delete('/user/:userId', UserController.remove)
-
     // get user by id
     app.get('/user/:userId', UserController.show)
-
     // get all user
     app.get('/users', UserController.index)
+
+    // Route สำหรับสมัครสมาชิก
+    app.post('/register', AuthenticationController.register)
+
 }
+
 
