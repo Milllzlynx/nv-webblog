6ee98017c0212039d9b5b61f2a2ca0d3183d738f
- <->
2026-02-12 16:15:18 +0700

    Ch11

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,9 @@
 const isAuthenController = require('./controllers/isAuthenController')
 const UserController = require('./controllers/UserController')
 const UserAuthenController = require('./controllers/UserAuthenController')
+const BlogController = require('./controllers/BlogController')
 
-module.exports = (app) => { 
+module.exports = (app) => {
     // create user
     app.post('/user', UserController.create)
     // edit user, suspend, active
@@ -17,6 +18,18 @@ module.exports = (app) => {
     // Route สำหรับสมัครสมาชิก
     app.post('/login', UserAuthenController.login)
     app.post('/register', UserAuthenController.register)
+
+    // blog route
+    // create blog
+    app.post('/blog', BlogController.create)
+    // edit blog, suspend, active
+    app.put('/blog/:blogId', BlogController.put)
+    // delete blog
+    app.delete('/blog/:blogId', BlogController.remove)
+    // get blog by id
+    app.get('/blog/:blogId', BlogController.show)
+    // get all blog
+    app.get('/blogs', BlogController.index)
 
 }
 
